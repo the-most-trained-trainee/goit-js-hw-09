4b8b5891d66a2322b97a7d2d35e5ceee21bde23e
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-30 22:31:12 +0300

    Interim Commit 30072022 (task 1/3))

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+stopButton.disabled = true;
+let actualInterbalId = 0;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function colorPublisher() {
+    document.querySelector("body").style.background = getRandomHexColor();
+};
+
+startButton.addEventListener("click", () => {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    actualInterbalId = setInterval(colorPublisher, 1000);
+});
+
+stopButton.addEventListener("click", () => {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(actualInterbalId);
+});
