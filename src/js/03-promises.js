38e829eccd26bb7bd886fe6b5fc8ec4b2efe9c36
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-31 17:31:52 +0300

    Interim Commit 31072022 (task 3/3))

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+createPromisesForm = document.querySelector(".form");
+
+createPromisesForm.addEventListener("submit", onSubmit);
+
+let formOutPut = undefined;
+
+function formDataReceipt() {
+  const formData = new FormData(createPromisesForm);
+  let formOutPut = {};
+  formData.forEach((value, name) => {
+    formOutPut[name] = value;
+  });
+  return formOutPut;
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  formOutPut = formDataReceipt(event);
+  for (let i = 0; i < formOutPut.amount; i++) {
+    createPromise(formOutPut.delay, formOutPut.step);
+  }
+  event.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
+
   const shouldResolve = Math.random() > 0.3;
+
+
+  const promise = new Promise();
+
+
+
   if (shouldResolve) {
     // Fulfill
   } else {
     // Reject
   }
 }
+
+
+// 43 17 
