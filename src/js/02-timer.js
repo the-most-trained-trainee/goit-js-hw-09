fcd3ca4825e6e9516cbd540cb319ff8e476ed56a
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-08-02 00:31:17 +0300

    Final Commit 02082022 (task 3/3)

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,83 +1,76 @@
+import Notiflix from 'notiflix';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from 'notiflix';
 
 const dataToCountInput = document.querySelector("#datetime-picker");
-
-let msSelectedTime = 0;
-let msTimeDiscrepancy = 0;
-let adaptedTimeDiscrepancy = 0;
-
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-
-    onClose(selectedDates) {
-        msSelectedTime = selectedDates[0].valueOf();
-        const currentDate = new Date();
-        const msCurrentTime = currentDate.getTime();
-        msTimeDiscrepancy = msSelectedTime - msCurrentTime;
-        if (msTimeDiscrepancy <= 0) {
-            Notiflix.Notify.warning("Please choose a date in the future")
-        } else {
-            startCountingButton.disabled = false;
-        }
-    },
-};
-
-const receivedDateTime = flatpickr(dataToCountInput, options);
-
-const startCountingButton = document.querySelector("[data-start]");
-startCountingButton.disabled = true;
-
 const displayDays = document.querySelector("[data-days]");
 const displayHours = document.querySelector("[data-hours]");
 const displayMinutes = document.querySelector("[data-minutes]");
 const displaySeconds = document.querySelector("[data-seconds]");
+const startCountingButton = document.querySelector("[data-start]");
+startCountingButton.disabled = true;
 
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+let msSelectedTime = 0;
 
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+const timeInput = {
+    msTimeDiscrepancy: 0,
+    options: {
+        enableTime: true,
+        time_24hr: true,
+        defaultDate: new Date(),
+        minuteIncrement: 1,
+        onClose(selectedDates) {
+            msSelectedTime = selectedDates[0].valueOf();
+            const currentDate = new Date();
+            const msCurrentTime = currentDate.getTime();
+            this.msTimeDiscrepancy = msSelectedTime - msCurrentTime;
+            if (this.msTimeDiscrepancy <= 0) {
+                Notiflix.Notify.warning("Please choose a date in the future")
+            } else {
+                startCountingButton.disabled = false;
+            }
+        },
+    },
+    make() {
+        flatpickr(dataToCountInput, this.options)
+    },
+};
 
-    return { days, hours, minutes, seconds };
-}
+const countdownTimer = {
 
-function addLeadingZero(value) {
-    return String(value).padStart(2, "0");
-}
+    convertMs(ms) {
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+        const days = Math.floor(ms / day);
+        const hours = Math.floor((ms % day) / hour);
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+        return { days, hours, minutes, seconds };
+    },
 
-startCountingButton.addEventListener("click", () => {
+    addLeadingZero(value) {
+        return String(value).padStart(2, "0");
+    },
 
-    let minusCheck = false;
+    start() {
+        this.buttonIntervalId = setInterval(() => {
+            const currentDateDisplay = new Date();
+            const msCurrentTimeDisplay = currentDateDisplay.getTime();
+            const msTimeDiscrepancyDisplay = msSelectedTime - msCurrentTimeDisplay;
+            adaptedTimeDiscrepancyDisplay = this.convertMs(msTimeDiscrepancyDisplay);
+            displayDays.textContent = this.addLeadingZero(adaptedTimeDiscrepancyDisplay.days);
+            displayHours.textContent = this.addLeadingZero(adaptedTimeDiscrepancyDisplay.hours);
+            displayMinutes.textContent = this.addLeadingZero(adaptedTimeDiscrepancyDisplay.minutes);
+            displaySeconds.textContent = this.addLeadingZero(adaptedTimeDiscrepancyDisplay.seconds);
+            if (msTimeDiscrepancyDisplay < 1000) {
+                clearInterval(this.buttonIntervalId);
+            }
+        }, 1000);
+    },
+};
 
-    let buttonIntervalId = setInterval(() => {
+timeInput.make();
 
-        const currentDateDisplay = new Date();
-        const msCurrentTimeDisplay = currentDateDisplay.getTime();
-        msTimeDiscrepancyDisplay = msSelectedTime - msCurrentTimeDisplay;
-        adaptedTimeDiscrepancyDisplay = convertMs(msTimeDiscrepancyDisplay);
-
-        displayDays.textContent = addLeadingZero(adaptedTimeDiscrepancyDisplay.days);
-        displayHours.textContent = addLeadingZero(adaptedTimeDiscrepancyDisplay.hours);
-        displayMinutes.textContent = addLeadingZero(adaptedTimeDiscrepancyDisplay.minutes);
-        displaySeconds.textContent = addLeadingZero(adaptedTimeDiscrepancyDisplay.seconds);
-
-        if (msTimeDiscrepancyDisplay < 1000) {
-            clearInterval(buttonIntervalId);
-        }
-    }, 1000);
-});
+startCountingButton.addEventListener("click", () => countdownTimer.start());
